d943de0bca522e04b54a9f006b36b3efbc8d8ef6
BBC_User <BBC_User@BBC-LAPTOP1>
2024-12-02 18:00:14 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp_lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ConsoleApp_lab1
+{
+    public class Program
+    {
+        public static double Calculate1(double x, double y, double a)
+        {
+            return 12 * a + ((a / y) / ((3 * a + x * a) / x)) * ((3 * x + 2 * a + a) / (3 * a + 2 * x - y)) + 12 * x;
+        }
+
+        public static double Calculate2(double x, double y, double a)
+        {
+            return 10 * x - ((3 * x + 2 * a - a) / (3 * a + 2 * x + y)) + ((a * y * x / (3 * y)) / (3 * a / x)) - 2 * x;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("значение x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("значение y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("значение a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            double z1 = Calculate1(x, y, a);
+            double z2 = Calculate2(x, y, a);
+
+            Console.WriteLine($"выражение 1: z = {z1}");
+            Console.WriteLine($"выражение 2: z = {z2}");
+        }
+    }
+}
+
---
+++ TestProject_lab1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using ConsoleApp_lab1;
+
+namespace TestProject_lab1
+{
+    public class ProgramTests
+    {
+        [Test]
+        public void TestExpression1()
+        {
+            double x = 1, y = 2, a = 3;
+            double expected = 48.167;
+            double result = Program.Calculate1(x, y, a);
+            Assert.AreEqual(expected, result, 0.001);
+        }
+
+        [Test]
+        public void TestExpression2()
+        {
+            double x = 1, y = 2, a = 3;
+            double expected = 7.649;
+            double result = Program.Calculate2(x, y, a);
+            Assert.AreEqual(expected, result, 0.001);
+        }
+    }
+}
